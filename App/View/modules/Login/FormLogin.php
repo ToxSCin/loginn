a8160a4be412b806914ed74901f1e026d7dd94c5
Renato (Tox) <renatoserrano.am@gmail.com>
2022-10-31 09:36:02 -0300

    Login Php

--- App/View/modules/Login/FormLogin.php
+++ App/View/modules/Login/FormLogin.php
@@ -1,8 +1,53 @@
-<form action="/login/auth" method="post" >
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista Categoria</title> <style>
+    input{
+        font-size: 40px;
+        background-color:whitesmoke;
+        font-family: 'Blaka', cursive;
+            font-family: 'Pacifico', cursive;
+    }
+
+    label,button,legend{
+        font-size: 40px;
+        font-family: 'Blaka', cursive;
+            font-family: 'Pacifico', cursive;
+    }
+
+    input,label {
+        border: 1px solid black;
+        border-collapse: collapse;
+        text-align: center;
+    }
+    body{
+            background-color: whitesmoke;
+        }
+    </style>
+</head>
+<body>
+    <center>    
+
+    
+    <form action="/login/auth" method="post" >
  
     <label>E-mail:</label>
     <input name="email" type="text" />
     <label>Senha:</label>
     <input name="senha" type="password" />
     <button type="submit">Entrar</button>
-</form>
+    </form>
+              
+            
+
+        </form>
+    </fieldset>
+          
+     </center>
+</body>
+</html>
+
