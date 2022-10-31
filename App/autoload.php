99c804b38a964758563e5a77186ad697d000b068
Renato (Tox) <renatoserrano.am@gmail.com>
2022-10-31 09:13:46 -0300

    Login Feito Com Sucesso

---
+++ App/autoload.php
@@ -0,0 +1,11 @@
+<?php
+spl_autoload_register(function ($nome_da_classe) 
+{
+    $arquivo = BASEDIR . '/' . $nome_da_classe . '.php';
+
+    if(file_exists($arquivo))
+    {
+        include $arquivo;
+    } else
+    exit('Not Found. Try Again: ' . $arquivo . "<br />");
+});
